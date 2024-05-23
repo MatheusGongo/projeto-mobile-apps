6d7afd80cad091442a052c5e9180b332960c42b4
matheusgongo <matheusgongo@ucl.br>
2024-05-23 15:34:42 -0300

    remove keys

--- serenemind/lib/config.dart
+++ serenemind/lib/config.dart
@@ -1,5 +1,5 @@
 class Config {
-  static const openaiApiKey = 'sk-proj-BWRUhNOysFkmFVEEzNQfT3BlbkFJSd6bSy6wR6uVrClCfU7Y';
-  static const spotifyClientId = '4d9977a3d0ab42e893b93c8c77a76e8b';
-  static const spotifyClientSecret = 'f8f98d96431a4cb48496a38cf26f2f3a';
+  static const openaiApiKey = '';
+  static const spotifyClientId = '';
+  static const spotifyClientSecret = '';
 }
